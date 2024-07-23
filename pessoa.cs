3964b8328cb7b57913faed448342658fd6a21003
luisplathdm <132583717+luisplathdm@users.noreply.github.com>
2024-07-23 11:40:32 -0300

    primeira parte

--- pessoa.cs
+++ pessoa.cs
@@ -5,7 +5,7 @@ namespace Fluxograma;
 
  public class Pessoa
 {
- protected Int64 id;
+ protected int id;
  protected string nome;
  protected string telefone;
  protected string cidade;
@@ -13,13 +13,52 @@ namespace Fluxograma;
  protected string uf;
  protected DateTime dn;
 
+ public void SetID (int NumeroDeIndentificacao)
+ {
+  id = NumeroDeIndentificacao;
+ }
+ 
+ public void GetID()
+ {
+   return id;
+ }
 
  public void SetNome (string NomeDaPessoa)
  {
    nome = NomeDaPessoa;
  }
+
  public string GetNome()
  {
     return nome;
  }
+
+ public void SetTelefone (string telefone)
+ {
+  this.telefone = telefone;
+ }
+
+ public string GetTelefone()
+ {
+   return telefone;
+ }
+
+ public void SteCidade (string cidade)
+ {
+   this.cidade = cidade;
+ }
+
+ public string GetCidade()
+ {
+   return cidade;
+ }
+  public void SetMunicipio (string municipio)
+  {
+    this.municipio = municipio;
+  }
+
+  public string GetMunicipio()
+  {
+    return municipio;
+  }
 }
